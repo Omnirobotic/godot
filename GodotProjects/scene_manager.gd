90516d02d9f83762a1338be32abb7b4ec57975bf
Ulric Villeneuve <ulric.villeneuve@omnirobotic.com>
2019-10-07 08:10:04 -0400

    Removed unwanted code. Removed comment in the scene. Not supported for now. Connected to all signals

--- GodotProjects/scene_manager.gd
+++ GodotProjects/scene_manager.gd
@@ -11,9 +11,13 @@ func _pressed():
 	if !is_checked:
 		print("Connecting...")
 		print(get_parent().get_name())
-		SceneManager.connect("update_joints", get_parent(), "move")
+		SceneManager.connect("update_joints", get_parent(), "update_joints")
+		SceneManager.connect("update_objects", get_parent(), "update_objects")
+		#SceneManager.connect("ios", get_parent(), "ios")
 		is_checked = true
 	else:
 		print("Disconnecting...")
-		SceneManager.disconnect("update_joints", get_parent(), "move")
+		SceneManager.disconnect("update_joints", get_parent(), "update_joints")
+		SceneManager.disconnect("update_objects", get_parent(), "update_objects")
+		#SceneManager.disconnect("ios", get_parent(), "ios")
 		is_checked = false
